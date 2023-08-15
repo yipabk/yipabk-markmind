0feaf80ecbc94debf9d959b22c512822c1640310
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2023-08-15 20:13:21 +0700

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import Landing from "./pages/Landing";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer'
+import { Navbar } from './components/Navbar'
+import Landing from './pages/Landing'
 
 function App() {
-  return (
-    <div className="flex flex-col">
-      <Navbar></Navbar>
-      <Landing></Landing>
-      <Footer></Footer>
-    </div>
-  );
+	return (
+		<div className='flex flex-col'>
+			<Navbar></Navbar>
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<Landing />} />
+				</Routes>
+			</BrowserRouter>
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
