0e08087636c8164a923be349ecf40057c443acfb
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2023-08-19 00:18:13 +0700

    changed header style logic

--- src/components/commons/FadingHeader.tsx
+++ src/components/commons/FadingHeader.tsx
@@ -3,16 +3,18 @@ import React from 'react'
 interface HeaderProps {
 	children?: React.ReactNode
 	bg: string
-	color?: string
+	variant: 'black' | 'blue'
 	className?: string
 }
 
-export const FadingHeader = ({ children = null, bg, color = 'black', className = '' }: HeaderProps) => {
+export const FadingHeader = ({ children = null, bg, className = '', variant }: HeaderProps) => {
 	return (
 		<div className={`relative overflow-hidden ${className}`}>
 			{children}
 			<div
-				className={`absolute w-full h-full z-[-1] bg-gradient-to-t from-${color} via-30% via-${color} to-transparent`}></div>
+				className={`absolute w-full h-full z-[-1] bg-gradient-to-t ${
+					variant === 'blue' ? 'from-blue' : 'from-black'
+				} to-transparent`}></div>
 			<img className='absolute w-full h-full object-cover z-[-2]' src={bg}></img>
 		</div>
 	)
