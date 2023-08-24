888cec766bf3b6fb720bf5b9768b00e72ddc11c1
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2023-08-24 22:16:19 +0700

    revamped footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,11 @@
+import { CallIcon } from './icons/CallIcon'
+import { MailIcon } from './icons/MailIcon'
+import instagram from '../assets/icon/instagram.png'
+import facebook from '../assets/icon/facebook.png'
+import youtube from '../assets/icon/youtube.png'
+import { LocationIcon } from './icons/LocationIcon'
+import dewaweb from '../assets/images/dewaweb.png'
+
 export const Footer = () => {
 	return (
 		<footer className='bg-white' aria-labelledby='footerHeading'>
@@ -7,7 +15,9 @@ export const Footer = () => {
 			<div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8'>
 				<div className='mb-12 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 xl:mb-0 text-xs md:text-base'>
 					<div className='flex'>
-						<div className='mr-4'>O</div>
+						<div className='mr-4'>
+							<LocationIcon />
+						</div>
 						<div>
 							<div>Gedung E-Building, Lantai 2</div>
 							<div>Komplek Ruko harmoni Plaza Blok I, No 1-4,</div>
@@ -16,33 +26,43 @@ export const Footer = () => {
 						</div>
 					</div>
 					<div>
-						<div className='flex'>
-							<div className='mr-4'>O</div>
+						<div className='flex items-center'>
+							<div className='mr-4'>
+								<MailIcon />
+							</div>
 							<div>yipabk@gmail.com</div>
 						</div>
-						<div className='flex'>
-							<div className='mr-4'>O</div>
+						<div className='flex items-center'>
+							<div className='mr-4'>
+								<CallIcon />
+							</div>
 							<div>+62 813-7070-3429</div>
 						</div>
 					</div>
 					<div>
 						<div className='font-bold'>KONTAK KAMI</div>
-						<div className='flex'>
-							<div className='mr-4'>O</div>
+						<div className='flex items-center'>
+							<div className='mr-4'>
+								<img src={instagram} alt='' />
+							</div>
 							<div>Instagram</div>
 						</div>
-						<div className='flex'>
-							<div className='mr-4'>O</div>
+						<div className='flex items-center'>
+							<div className='mr-4'>
+								<img src={youtube} alt='' />
+							</div>
 							<div>Youtube</div>
 						</div>
-						<div className='flex'>
-							<div className='mr-4'>O</div>
+						<div className='flex items-center'>
+							<div className='mr-4'>
+								<img src={facebook} alt='' />
+							</div>
 							<div>Facebook</div>
 						</div>
 					</div>
 					<div className='flex flex-col lg:col-span-2 lg:items-center'>
 						<p className='text-gray-500 text-base font-bold uppercase'>Didukung oleh</p>
-						<img className='h-10' src='/img/logos/workflow-mark-gray-300.svg' alt='Company name' />
+						<img className='h-[90px]' src={dewaweb} alt='Company name' />
 					</div>
 				</div>
 				<div className='mt-12 border-t border-gray-200 pt-8'>
---
+++ src/components/icons/MailIcon.tsx
@@ -0,0 +1,9 @@
+
+export const MailIcon = () => {
+	return (
+		<svg xmlns='http://www.w3.org/2000/svg' width='18' height='14' viewBox='0 0 18 14' fill='none'>
+			<path d='M16.1214 0.000976562H1.51562L8.81853 8.32351L16.1214 0.000976562Z' fill='#282828' />
+			<path d='M8.8185 11.0674L0 1.01465V13.5686H17.637V1.01465L8.8185 11.0674Z' fill='#282828' />
+		</svg>
+	)
+}
