8c6bc60796e2bc46066c20825f8ac526c4de2b9d
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2023-08-24 22:22:28 +0700

    add aos

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Footer } from "./components/Footer";
-import { Navbar } from "./components/Navbar/Navbar";
-import LandingPage from "./pages/LandingPage/LandingPage";
-import { Kontribusi } from "./pages/Kontribusi";
-import { TentangKami } from "./pages/TentangKami";
-import { Partner } from "./pages/Partner";
-import { Publikasi } from "./pages/Publikasi";
-import "aos/dist/aos.css"; // You can also use <link> for styles
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Footer } from './components/Footer'
+import { Navbar } from './components/Navbar/Navbar'
+import LandingPage from './pages/LandingPage/LandingPage'
+import { Kontribusi } from './pages/Kontribusi'
+import { TentangKami } from './pages/TentangKami'
+import { Partner } from './pages/Partner'
+import { Publikasi } from './pages/Publikasi'
+import AOS from 'aos'
+import 'aos/dist/aos.css' // You can also use <link> for styles
 
 function App() {
-  return (
-    <div className="flex flex-col">
-      <BrowserRouter>
-        <Navbar></Navbar>
-        <Routes>
-          <Route index element={<LandingPage />} />
-          <Route path="kontribusi" element={<Kontribusi />} />
-          <Route path="tentang-kami" element={<TentangKami />} />
-          <Route path="partner" element={<Partner />} />
-          <Route path="publikasi" element={<Publikasi />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
+	AOS.init()
+
+	return (
+		<div className='flex flex-col'>
+			<BrowserRouter>
+				<Navbar></Navbar>
+				<Routes>
+					<Route index element={<LandingPage />} />
+					<Route path='kontribusi' element={<Kontribusi />} />
+					<Route path='tentang-kami' element={<TentangKami />} />
+					<Route path='partner' element={<Partner />} />
+					<Route path='publikasi' element={<Publikasi />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</div>
+	)
 }
 
-export default App;
+export default App
