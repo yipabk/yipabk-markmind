d17e1bc4d73d2eb41f019f4ac4dacc36502cf595
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2023-08-28 00:10:08 +0700

    add icon and title

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -96,7 +96,7 @@ export const Navbar = () => {
 								<div className='pt-5 pb-6 px-5'>
 									<div className='flex items-center justify-between'>
 										<div>
-											<img className='h-8 w-auto' src='/img/logos/workflow-mark-indigo-600.svg' alt='Workflow' />
+											<Logo />
 										</div>
 										<div className='-mr-2'>
 											<Popover.Button className='bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
