f78b40fb9b0ae9df5e402f05366d7faf28bad296
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2023-09-01 23:58:51 +0700

    fixed partner page

--- src/components/commons/Partners.tsx
+++ src/components/commons/Partners.tsx
@@ -11,6 +11,7 @@ import partner3 from '../../assets/images/partner-3.png'
 import peace from '../../assets/images/peace.png'
 import shiningStars from '../../assets/images/shining-stars.png'
 import shiningStars2 from '../../assets/images/shining-stars-2.png'
+import { useLocation } from 'react-router-dom'
 
 interface Props {
 	title: string
@@ -20,10 +21,13 @@ interface Props {
 export const Partners = ({ title, subtitle }: Props) => {
 	const images = [dewaweb, kemenristek, partner1, hope, peace]
 	const images2 = [cae, akr, partner2, shiningStars, brainfit, shiningStars2, partner3, kokoSemesta]
+	const { pathname } = useLocation()
 
 	return (
 		<div className='flex flex-col items-center'>
-			<div className='font-display text-center text-3xl lg:text-5xl'>{title}</div>
+			<div className={`${pathname === '/' ? 'font-display' : 'font-montserrat'} text-center text-3xl lg:text-5xl`}>
+				{title}
+			</div>
 			<div className='font-montserrat text-center text-3xl lg:text-5xl font-bold mt-4'>{subtitle}</div>
 			<div className='flex flex-col gap-4 mt-8'>
 				<div className='flex flex-wrap justify-center items-center gap-6'>
