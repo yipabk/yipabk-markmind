02076c2c5210e4e35e7b42dccc2cba7b336c03c0
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2023-09-02 00:11:35 +0700

    fixed artikel page

--- src/pages/Publikasi/Artikel/Layout.tsx
+++ src/pages/Publikasi/Artikel/Layout.tsx
@@ -27,11 +27,11 @@ export const Layout = ({
 				<div className='text-2xl lg:text-[32px] font-bold'>{firstSectionTitle}</div>
 				<div className='whitespace-pre-line text-justify'>{firstSectionContent}</div>
 			</div>
-			<div className='flex flex-col bg-blue h-[20vh]'>
+			<div className='flex flex-col bg-blue/40 h-[20vh]'>
 				<div className='border border-white bg-white w-[85%] h-[10vh]'></div>
 				<div className='border border-white bg-white w-[70%] h-[10vh]'></div>
 			</div>
-			<div className='flex flex-col bg-blue px-8 sm:px-20 lg:px-48 font-montserrat gap-12 pt-16 pb-52'>
+			<div className='flex flex-col bg-blue/40 px-8 sm:px-20 lg:px-48 font-montserrat gap-12 pt-16 pb-52'>
 				<div data-aos='fade-down' data-aos-once='true' className='text-2xl lg:text-[32px] font-bold'>
 					{secondSectionTitle}
 				</div>
