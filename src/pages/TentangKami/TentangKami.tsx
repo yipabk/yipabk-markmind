f26ca44287783a3c720c97c7f89e89fd7942eca4
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2023-10-09 23:14:47 +0700

    adjusted padding

--- src/pages/TentangKami/TentangKami.tsx
+++ src/pages/TentangKami/TentangKami.tsx
@@ -78,7 +78,7 @@ export const TentangKami = () => {
 		if (width >= 1280) {
 			return 3
 		}
-		if (width >= 640) {
+		if (width >= 680) {
 			return 2
 		}
 
@@ -91,134 +91,106 @@ export const TentangKami = () => {
 		setMaxItem(getMaxItem())
 	}, [width, setMaxItem])
 
-  return (
-    <div>
-      <FadingHeader
-        variant="black"
-        className="flex justify-center items-end w-full h-[42vh] mt-20"
-        bg={hero}
-      >
-        <div
-          data-aos="fade-up"
-          data-aos-once="true"
-          className="text-white font-display text-5xl mb-12 tracking-wide"
-        >
-          TENTANG KAMI
-        </div>
-      </FadingHeader>
-      <div>
-        <div
-          data-aos="fade-down"
-          data-aos-once="true"
-          data-aos-delay="200"
-          className="px-6 sm:px-12 md:px-24 lg:px-56 w-full py-12 flex justify-center items-center flex-col font-montserrat font-[400]"
-        >
-          <div className="mb-24">
-            <div className="mb-6 leading-6 text-justify">
-              Yayasan Indonesia Peduli Anak Berkebutuhan Khusus (YIPABK) berdiri
-              tahun 2012 dengan akta notaris Linda Herawati Nomor 77 tanggal 28
-              Juni 2012.
-            </div>
-            <div className="mb-6 leading-6 text-justify">
-              YIPABK lahir dari kepedulian sekelompok pendidik (guru, orangtua,
-              dan pemerhati anak) terhadap masa depan anak-anak yang memiliki
-              kondisi kekhususan. Sebab begitu besar jumlah anak-anak di
-              Indonesia, baik yang mengalami ketunaan fisik, mental,
-              intelegensi/kognisi, maupun sosial atau gangguan perilaku, yang
-              masih belum mendapat perhatian terhadap kebutuhan penanganan dan
-              pendidikan mereka.
-            </div>
-            <div className="mb-6 leading-6 text-justify">
-              Cikal bakal YIPABK dimulai dari gerakan bersama para pendidik
-              dalam memperhatikan pentingnya penanganan yang tepat dan
-              tersedianya layanan pendidikan yang baik sejak dini untuk membantu
-              anak-anak berkondisi kekhususan tersebut memperoleh kesempatan
-              bertumbuh dan berkembang, dan memiliki hidup yang lebih baik.
-            </div>
-          </div>
-        </div>
-        <div className="px-6 sm:px-12 md:px-24 lg:px-56 w-full h-full mb-24 py-16">
-          <div className="flex flex-col xl:flex-row mb-16 gap-6 lg:gap-16">
-            <img
-              data-aos="fade-right"
-              data-aos-once="true"
-              className="w-96 h-64 object-cover grayscale"
-              src={visi}
-              alt=""
-            />
-            <div data-aos="fade-left" data-aos-once="true" className="relative">
-              <div className="font-display text-4xl tracking-wide mb-6">
-                VISI
-              </div>
-              <div className="font-montserrat">
-                “Terwujudnya Indonesia dengan anak-anak yang bahagia dan
-                memiliki kesempatan pendidikan yang sama.”
-              </div>
-              <div className="bg-white w-[5.5rem] h-[4.5rem] absolute top-0 left-[-5.5rem]"></div>
-              <div className="bg-orange w-[2rem] h-[2rem] absolute top-[2rem] left-[-5rem]"></div>
-              <div className="bg-orange w-[14rem] h-[0.14rem] absolute top-[2.93rem] left-[-5rem]"></div>
-            </div>
-          </div>
-          <div className="flex flex-col xl:flex-row gap-6 lg:gap-16">
-            <img
-              data-aos="fade-right"
-              data-aos-once="true"
-              className="w-96 h-64 object-cover grayscale"
-              src={misi}
-              alt=""
-            />
-            <div data-aos="fade-left" data-aos-once="true" className="relative">
-              <div className="font-display text-4xl tracking-wide mb-6">
-                MISI
-              </div>
-              <div className="font-montserrat">
-                <div className="font-bold mb-4 ">
-                  Advocating, Creating, Educating Special Needs Children in
-                  Indonesia
-                </div>
-                <ol className="ml-4 list-decimal text-justify">
-                  <li>
-                    Advocate - Anak-anak yang memiliki keterbatasan dalam
-                    perkembangan maupun akses layanan pendidikan
-                  </li>
-                  <li>
-                    Create - Menciptakan kesempatan, ruang/fasilitas,
-                    lingkungan, pelayanan, pendidikan dan lapangan pekerjaan
-                    bagi anak-anak berkebutuhan khusus
-                  </li>
-                  <li>
-                    Educate - Mengedukasi seluruh elemen masyarakat melalui
-                    berbagai kegiatan agar memiliki kepedulian, kesadaran, dan
-                    pemahaman yang baik terhadap penanganan dan pendidikan
-                    anak-anak berkebutuhan khusus
-                  </li>
-                </ol>
-              </div>
-              <div className="bg-white w-[5.5rem] h-[4.5rem] absolute top-0 left-[-5.5rem]"></div>
-              <div className="bg-orange w-[2rem] h-[2rem] absolute top-[2rem] left-[-5rem]"></div>
-              <div className="bg-orange w-[14rem] h-[0.14rem] absolute top-[2.93rem] left-[-5rem]"></div>
-            </div>
-          </div>
-        </div>
-        <div className="w-full h-full py-24 px-4 sm:px-6 md:px-12 lg:px-24 items-center flex flex-col xl:flex-row gap-12">
-          <div
-            data-aos="fade-right"
-            data-aos-once="true"
-            className="xl:mr-auto text-4xl ml-4 xl:ml-16 whitespace-pre-line text-center xl:text-start font-display underline underline-offset-8 decoration-orange tracking-wide"
-          >
-            {`Meet Our Team`}
-          </div>
-          <Slider<TeamCardProps>
-            key={maxItem}
-            items={peoples}
-            maxItemPerView={maxItem}
-            showPagination
-            renderItem={(item) => (
-              <TeamCard key={`${item.title}-${item.id}`} {...item} />
-            )}
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
+	return (
+		<div>
+			<FadingHeader variant='black' className='flex justify-center items-end w-full h-[42vh] mt-20' bg={hero}>
+				<div data-aos='fade-up' data-aos-once='true' className='text-white font-display text-5xl mb-12 tracking-wide'>
+					TENTANG KAMI
+				</div>
+			</FadingHeader>
+			<div>
+				<div
+					data-aos='fade-down'
+					data-aos-once='true'
+					data-aos-delay='200'
+					className='px-6 sm:px-12 md:px-24 lg:px-56 w-full py-12 flex justify-center items-center flex-col font-montserrat font-[400]'>
+					<div className='mb-24'>
+						<div className='mb-6 leading-6 text-justify'>
+							Yayasan Indonesia Peduli Anak Berkebutuhan Khusus (YIPABK) berdiri tahun 2012 dengan akta notaris Linda
+							Herawati Nomor 77 tanggal 28 Juni 2012.
+						</div>
+						<div className='mb-6 leading-6 text-justify'>
+							YIPABK lahir dari kepedulian sekelompok pendidik (guru, orangtua, dan pemerhati anak) terhadap masa depan
+							anak-anak yang memiliki kondisi kekhususan. Sebab begitu besar jumlah anak-anak di Indonesia, baik yang
+							mengalami ketunaan fisik, mental, intelegensi/kognisi, maupun sosial atau gangguan perilaku, yang masih
+							belum mendapat perhatian terhadap kebutuhan penanganan dan pendidikan mereka.
+						</div>
+						<div className='mb-6 leading-6 text-justify'>
+							Cikal bakal YIPABK dimulai dari gerakan bersama para pendidik dalam memperhatikan pentingnya penanganan
+							yang tepat dan tersedianya layanan pendidikan yang baik sejak dini untuk membantu anak-anak berkondisi
+							kekhususan tersebut memperoleh kesempatan bertumbuh dan berkembang, dan memiliki hidup yang lebih baik.
+						</div>
+					</div>
+				</div>
+				<div className='px-6 sm:px-12 md:px-24 lg:px-56 w-full h-full mb-24 py-16'>
+					<div className='flex flex-col xl:flex-row mb-16 gap-6 lg:gap-16'>
+						<img
+							data-aos='fade-right'
+							data-aos-once='true'
+							className='w-96 h-64 object-cover grayscale'
+							src={visi}
+							alt=''
+						/>
+						<div data-aos='fade-left' data-aos-once='true' className='relative'>
+							<div className='font-display text-4xl tracking-wide mb-6'>VISI</div>
+							<div className='font-montserrat'>
+								“Terwujudnya Indonesia dengan anak-anak yang bahagia dan memiliki kesempatan pendidikan yang sama.”
+							</div>
+							<div className='bg-white w-[5.5rem] h-[4.5rem] absolute top-0 left-[-5.5rem]'></div>
+							<div className='bg-orange w-[2rem] h-[2rem] absolute top-[2rem] left-[-5rem]'></div>
+							<div className='bg-orange w-[14rem] h-[0.14rem] absolute top-[2.93rem] left-[-5rem]'></div>
+						</div>
+					</div>
+					<div className='flex flex-col xl:flex-row gap-6 lg:gap-16'>
+						<img
+							data-aos='fade-right'
+							data-aos-once='true'
+							className='w-96 h-64 object-cover grayscale'
+							src={misi}
+							alt=''
+						/>
+						<div data-aos='fade-left' data-aos-once='true' className='relative'>
+							<div className='font-display text-4xl tracking-wide mb-6'>MISI</div>
+							<div className='font-montserrat'>
+								<div className='font-bold mb-4 '>
+									Advocating, Creating, Educating Special Needs Children in Indonesia
+								</div>
+								<ol className='ml-4 list-decimal text-justify'>
+									<li>
+										Advocate - Anak-anak yang memiliki keterbatasan dalam perkembangan maupun akses layanan pendidikan
+									</li>
+									<li>
+										Create - Menciptakan kesempatan, ruang/fasilitas, lingkungan, pelayanan, pendidikan dan lapangan
+										pekerjaan bagi anak-anak berkebutuhan khusus
+									</li>
+									<li>
+										Educate - Mengedukasi seluruh elemen masyarakat melalui berbagai kegiatan agar memiliki kepedulian,
+										kesadaran, dan pemahaman yang baik terhadap penanganan dan pendidikan anak-anak berkebutuhan khusus
+									</li>
+								</ol>
+							</div>
+							<div className='bg-white w-[5.5rem] h-[4.5rem] absolute top-0 left-[-5.5rem]'></div>
+							<div className='bg-orange w-[2rem] h-[2rem] absolute top-[2rem] left-[-5rem]'></div>
+							<div className='bg-orange w-[14rem] h-[0.14rem] absolute top-[2.93rem] left-[-5rem]'></div>
+						</div>
+					</div>
+				</div>
+				<div className='w-full h-full py-24 px-4 sm:px-6 md:px-12 items-center flex flex-col xl:flex-row gap-12'>
+					<div
+						data-aos='fade-right'
+						data-aos-once='true'
+						className='xl:mr-auto text-4xl ml-4 whitespace-pre-line text-center xl:text-start font-display underline underline-offset-8 decoration-orange tracking-wide'>
+						{`Meet Our Team`}
+					</div>
+					<Slider<TeamCardProps>
+						key={maxItem}
+						items={peoples}
+						maxItemPerView={maxItem}
+						showPagination
+						renderItem={(item) => <TeamCard key={`${item.title}-${item.id}`} {...item} />}
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
